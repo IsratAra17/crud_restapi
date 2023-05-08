220cecf648df16650d3da8ea5b0da62cc34f8427
s.m.israt <smisratara25@gmail.com>
2023-05-08 21:01:22 +0600

    create a style for Textfield input as AppInputDecoration

--- lib/Style/style.dart
+++ lib/Style/style.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
 
 const colorRed=Color.fromRGBO (231, 28, 36, 1);
 const colorGreen=Color.fromRGBO(33, 191, 115, 1);
 const colorWhite=Color.fromRGBO(255, 255, 255, 1);
 const colorDarkBlue=Color.fromRGBO(44, 62, 80, 1);
+
+InputDecoration AppInputDecoration(label){
+  return InputDecoration(
+    focusedBorder: const OutlineInputBorder(
+      borderSide: const BorderSide(color: colorGreen,width: 0)
+    ),
+border: OutlineInputBorder(),
+    labelText: label
+
+  );
+}
