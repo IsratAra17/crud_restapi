d12ef34f153e46bfac9de3eb07e0884b455f9657
s.m.israt <smisratara25@gmail.com>
2023-05-10 10:53:07 +0600

    DropdownButton Error solved

--- lib/Screen/productCreateScreen.dart
+++ lib/Screen/productCreateScreen.dart
@@ -1,3 +1,4 @@
+
 import 'package:crud_restapi/Style/style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,7 @@ class _ProductCreateScreenState extends State<ProductCreateScreen> {
         title: Text("Create Product "),),
         body:Stack(
           children: [
+            ScreenBackground(context),
             Container(
               child: SingleChildScrollView(
                 padding: EdgeInsets.all(20),
@@ -29,7 +31,43 @@ class _ProductCreateScreenState extends State<ProductCreateScreen> {
                   SizedBox(height: 20),
                   TextField(onChanged: (value){},decoration: AppInputDecoration("Unit Price"),),
                   SizedBox(height: 20),
-                  TextField(onChanged: (value){},decoration: AppInputDecoration("Total Price"),)
+                  TextField(onChanged: (value){},decoration: AppInputDecoration("Total Price"),),
+                  SizedBox(height: 20),
+
+                  // AppDropDownStyle(
+                  //     DropdownButton(
+                  //       value:" " ,
+                  //       items:[
+                  //         DropdownMenuItem(child: Text('Select Qt'),value: "",),
+                  //         DropdownMenuItem(child: Text('1 pcs'),value: "1 pcs",),
+                  //         DropdownMenuItem(child: Text('2 pcs'),value: '2 pcs',),
+                  //         DropdownMenuItem(child: Text('3 pcs'),value: '3 pcs',),
+                  //         DropdownMenuItem(child: Text('4 pcs'),value: '4 pcs',),
+                  //       ],
+                  //       onChanged: (value){
+                  //
+                  //       },
+                  //       underline: Container(),
+                  //       isExpanded: true,
+                  //     )
+                  // ),
+AppDropDownStyle(
+                  DropdownButton(items: [
+                    DropdownMenuItem(child: Text('Select Qt'),value: "",),
+                           DropdownMenuItem(child: Text('1 pcs'),value: "1 pcs",),
+                            DropdownMenuItem(child: Text('2 pcs'),value: '2 pcs',),
+                             DropdownMenuItem(child: Text('3 pcs'),value: '3 pcs',),
+                             DropdownMenuItem(child: Text('4 pcs'),value: '4 pcs',),
+                  ], onChanged: (value){},
+                  underline: Container(),
+                    isExpanded: true,
+
+                  ),),
+                  SizedBox(height: 20),
+Container(child: ElevatedButton(style: AppButtonStyle(),
+  onPressed: (){}, child: SuccessButtonChild("Submit"),
+),)
+
                 ],),
               )
             )
--- lib/Style/style.dart
+++ lib/Style/style.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 const colorRed=Color.fromRGBO (231, 28, 36, 1);
 const colorGreen=Color.fromRGBO(33, 191, 115, 1);
 const colorWhite=Color.fromRGBO(255, 255, 255, 1);
 const colorDarkBlue=Color.fromRGBO(44, 62, 80, 1);
 
+SvgPicture ScreenBackground(context)
+{
+  return SvgPicture.asset(
+    'assets/images/screen-back.svg',
+    alignment:Alignment.center ,
+    width:MediaQuery.of(context).size.width ,
+    height:MediaQuery.of(context).size.width ,
+    fit: BoxFit.cover,
+
+
+  );
+}
 InputDecoration AppInputDecoration(label){
   return InputDecoration(
     focusedBorder: const OutlineInputBorder(
@@ -14,5 +28,51 @@ InputDecoration AppInputDecoration(label){
 border: OutlineInputBorder(),
     labelText: label
 
+  );
+}
+ DecoratedBox AppDropDownStyle(child)
+ {
+   return DecoratedBox(
+    decoration: BoxDecoration(
+      color: colorWhite,
+      border:Border.all(color:colorDarkBlue,width: 1),
+      borderRadius: BorderRadius.circular(4),
+    ),
+     child:Padding(
+       padding: EdgeInsets.only(left: 30,right: 30),
+       child: child,
+
+     ),
+
+
+   );
+ }
+
+
+
+ButtonStyle AppButtonStyle(){
+  return ElevatedButton.styleFrom(
+      elevation: 1,
+      padding: EdgeInsets.all(0),
+      backgroundColor: Colors.transparent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6))
+  );
+}
+
+
+Ink SuccessButtonChild(ButtonText){
+  return Ink(
+    decoration: BoxDecoration(
+      color: colorGreen,
+      borderRadius: BorderRadius.circular(6),
+    ),
+    child: Container(
+      height: 45,
+      alignment: Alignment.center,
+      child: Text(ButtonText,style: TextStyle(
+          fontWeight: FontWeight.w400,
+          fontSize: 15
+      ),),
+    ),
   );
 }
