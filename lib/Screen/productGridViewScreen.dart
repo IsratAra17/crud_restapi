e4594701a0426f40685d07d91064b86b53b1876b
s.m.israt <smisratara25@gmail.com>
2023-05-14 19:05:54 +0600

    ProductGridView

---
+++ lib/Screen/productGridViewScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductGridViewScreen extends StatefulWidget {
+  const ProductGridViewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProductGridViewScreen> createState() => _ProductGridViewScreenState();
+}
+
+class _ProductGridViewScreenState extends State<ProductGridViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
