b8c96dba35fb69e3373964b8da47579db6f3d8d9
s.m.israt <smisratara25@gmail.com>
2023-05-15 20:34:55 +0600

    Delete product api added

--- lib/RestApi/restClient.dart
+++ lib/RestApi/restClient.dart
@@ -2,6 +2,37 @@
 import 'dart:convert';
 import 'package:crud_restapi/Style/style.dart';
 import 'package:http/http.dart'as http;
+
+
+Future<bool>ProductDeleteRequest(id)async{
+  var URL=Uri.parse("https://crud.teamrabbil.com/api/v1/DeleteProduct/"+id);
+
+  var PostHeader = {"content-Type": "application/json"};
+  var response = await http.get(URL, headers: PostHeader);
+
+  var ResultCode=response.statusCode;
+  var ResultBody=json.decode(response.body);
+
+  if(ResultCode==200 && ResultBody['status']=='success')
+  {
+    SuccessToast("Request Success!");
+
+    return true;
+  }
+  else
+  {
+    ErrorToast("Request Failed! Try again!!");
+    return false;
+  }
+}
+
+
+
+
+
+
+
+
 //Rest api Read the data from server
 Future<List>ProductGridViewListRequest()async{
   {
